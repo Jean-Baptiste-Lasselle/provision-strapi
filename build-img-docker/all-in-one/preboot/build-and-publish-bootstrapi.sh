905d6bab80bceba5757b17f7a09d3ace13cc7e1c
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.it@gmail.com>
2018-08-26 17:55:41 -0400

    Ajout d'un premier docker-compose, qui permet de lancer un registry npm privé, et un contneur réalisant le build et le publish de strapi à parti de son code source, pour el déployer ensuite dans un dernier conteneur, appelé "bootstrapi" + corrections syntaxe dockercomposefile

--- build-img-docker/all-in-one/preboot/build-and-publish-bootstrapi.sh
+++
@@ -1,4 +0,0 @@
-#!/bion/bash
-# - parceque le publish doit être fait au run, après le buiild, et lorsque la conteneur npm-registry est up n running
-RUN npm pack
-RUN npm publish --registry $URI_PRIVATE_NPM_REGISTRY
